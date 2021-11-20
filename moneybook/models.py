e1167f7c1cd32b321e616da6de54b2728749addb
KyeongHwanMin <h45652@naver.com>
2021-11-20 11:43:47 +0900

    feat: 가계부 앱을 추가하고, 지출에 대한 모델을 추가한다.

--- moneybook/models.py
+++ moneybook/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    memo = models.TextField()
+    spent_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
