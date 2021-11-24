8e0ed7b83dffe9705d8ae505ca19547e13d91080
KyeongHwanMin <h45652@naver.com>
2021-11-24 18:38:50 +0900

    feat:DDL파일 작성

--- config/settings.py
+++ config/settings.py
@@ -69,22 +69,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'db_name',
-#         'USER': 'db_user',
-#         'PASSWORD': 'password',
-#         'HOST': '127.0.0.1',
-#         'PORT': '13306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '13306',
+    }
+}
 
 
 # Password validation
